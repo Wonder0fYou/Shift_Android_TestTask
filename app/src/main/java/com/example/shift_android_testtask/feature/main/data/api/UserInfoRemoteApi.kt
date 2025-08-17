bc682d27238c831f666661980d10d14f286e9c69
Dmitry Makeev <dmtdmt2001@gmail.com>
2025-08-17 05:21:25 +0300

    поправил запрос + добавил permission

--- app/src/main/java/com/example/shift_android_testtask/feature/main/data/api/UserInfoRemoteApi.kt
+++ app/src/main/java/com/example/shift_android_testtask/feature/main/data/api/UserInfoRemoteApi.kt
@@ -2,8 +2,11 @@ package com.example.shift_android_testtask.feature.main.data.api
 
 import com.example.shift_android_testtask.core.network.model.UserResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface UserInfoRemoteApi {
-    @GET
-    suspend fun getUsers() : UserResponse
+    @GET(".")
+    suspend fun getUsers(
+        @Query("results") results: Int = 20
+    ) : UserResponse
 }
