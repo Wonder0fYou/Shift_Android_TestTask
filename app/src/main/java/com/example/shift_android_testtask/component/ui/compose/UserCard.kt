a5b4bd22e9040681cbca6288090c1d8cdb1cebff
Dmitry Makeev <dmtdmt2001@gmail.com>
2025-08-21 03:36:35 +0300

    подредактировал карточку профиля на главном экране

--- app/src/main/java/com/example/shift_android_testtask/component/ui/compose/UserCard.kt
+++ app/src/main/java/com/example/shift_android_testtask/component/ui/compose/UserCard.kt
@@ -2,9 +2,14 @@ package com.example.shift_android_testtask.component.ui.compose
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun UserCard(
@@ -19,13 +24,21 @@ fun UserCard(
         modifier = modifier
             .clickable { onUserClick() }
     ) {
-        Column(
+        Row(
             modifier = Modifier
         ) {
-            UiText(text = imageUrl)
-            UiText(text = address)
-            UiText(text = phone)
-            UiText(text = fcs)
+            WrapperImage(
+                model = imageUrl,
+                contentDescription = "фото юзера",
+                modifier = Modifier
+                    .size(128.dp)
+                    .clip(CircleShape),
+            )
+            Column {
+                UiText(text = address)
+                UiText(text = phone)
+                UiText(text = fcs)
+            }
         }
     }
 }
